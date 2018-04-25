97eab709eb1620df5f65d50bc2767aad6c3e453d
wojdave <38599297+wojdave@users.noreply.github.com>
2018-04-25 00:32:47 -0400

    delete Start_ROAM_Services.bat

--- Start_ROAM_Services.bat
+++
@@ -1,5 +0,0 @@
-@echo
-wmic service where "displayName like 'SLM_QM'" call startservice
-wmic service where "displayName like 'SLM_Proxy'" call startservice
-wmic service where "displayName like 'SLM_AS'" call startservice
-wmic service where "displayName like 'ROAM%%'" call startservice
