fae1654e28861b07c0b1aeec32c36ba646c27037
wojdave <38599297+wojdave@users.noreply.github.com>
2018-04-24 23:17:36 -0400

    added Restart2.2b.cmd

---
+++ Restart2.2b.cmd
@@ -0,0 +1,52 @@
+net stop "ROAM DCMAlarmService"
+
+net stop "ROAM DCMDataService"
+
+net stop "ROAM DiagnosticService"
+
+net stop "ROAM EventCommandService"
+
+net stop "ROAM IDCDataService"
+
+net stop "ROAM ImmediateCommandRefreshService"
+
+net stop "ROAM Mesh Noc Service"
+
+net stop "ROAM ResponseService"
+
+net stop "ROAM ROAMSensorAlarmService"
+
+net stop "ROAM ScheduleCommandService"
+
+net stop "ROAM SLMAlarmService"
+
+net stop "ROAM SLMDataService"
+
+
+
+
+
+net start "ROAM DCMAlarmService"
+
+net start "ROAM DCMDataService"
+
+net start "ROAM DiagnosticService"
+
+net start "ROAM EventCommandService"
+
+net start "ROAM IDCDataService"
+
+net start "ROAM ImmediateCommandRefreshService"
+
+net start "ROAM Mesh Noc Service"
+
+net start "ROAM ResponseService"
+
+net start "ROAM ROAMSensorAlarmService"
+
+net start "ROAM ScheduleCommandService"
+
+net start "ROAM SLMAlarmService"
+
+net start "ROAM SLMDataService"
+
