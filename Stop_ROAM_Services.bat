24ef053ea116b17b8e7097481aa5afd16aaae5d6
wojdave <38599297+wojdave@users.noreply.github.com>
2018-04-25 00:33:10 -0400

    delete Stop_ROAM_Services.bat

--- Stop_ROAM_Services.bat
+++
@@ -1,6 +0,0 @@
-@echo
-wmic service where "displayName like 'ROAM%%'" call stopservice
-wmic service where "displayName like 'SLM_AS'" call stopservice
-wmic service where "displayName like 'SLM_Proxy'" call stopservice
-wmic service where "displayName like 'SLM_QM'" call stopservice
-
