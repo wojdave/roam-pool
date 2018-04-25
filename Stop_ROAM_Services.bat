746a59da7f04c1de012be723059e6889fcb44ea3
wojdave <38599297+wojdave@users.noreply.github.com>
2018-04-25 00:39:16 -0400

    Update Stop_ROAM_Services.bat

--- Stop_ROAM_Services.bat
+++ Stop_ROAM_Services.bat
@@ -3,4 +3,3 @@ wmic service where "displayName like 'ROAM%%'" call stopservice
 wmic service where "displayName like 'SLM_AS'" call stopservice
 wmic service where "displayName like 'SLM_Proxy'" call stopservice
 wmic service where "displayName like 'SLM_QM'" call stopservice
-
